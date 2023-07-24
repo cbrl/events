574e39b9a2c3a72ef285b85303874dd162594465
Connor Broyles <cbroyles94@gmail.com>
2023-07-24 12:37:25 -0400

    Eliminate manual execution state tracking

--- include/events/detail/async_drop_signal_handler.hpp
+++ include/events/detail/async_drop_signal_handler.hpp
@@ -50,14 +50,9 @@ public:
 private:
 	using alloc_traits = std::allocator_traits<AllocatorT>;
 
-	using element_type = std::function<function_type>;
+	using element_type = std::shared_ptr<std::function<function_type>>;
 	using container_allocator_type = typename alloc_traits::template rebind_alloc<element_type>;
 	using container_type = plf::colony<element_type, container_allocator_type>;
-
-	using working_key_type = typename container_type::const_pointer;
-	using working_element_type = std::pair<working_key_type const, bool>;
-	using working_container_allocator_type = typename alloc_traits::template rebind_alloc<working_element_type>;
-	using working_container_type = std::map<working_key_type, bool, std::less<>, working_container_allocator_type>;
 
 	using remove_element_type = typename container_type::const_pointer;
 	using remove_container_allocator_type = typename alloc_traits::template rebind_alloc<remove_element_type>;
@@ -127,7 +122,6 @@ public:
 		}
 
 		callbacks = container_type{allocator};
-		working_callbacks = working_container_type{allocator};
 		to_remove = remove_container_type{allocator};
 
 		other.remove_invalidated_callbacks();
@@ -173,7 +167,7 @@ public:
 		auto locks = std::scoped_lock{other.callback_mut, other.to_remove_mut};
 
 		assert(
-			std::ranges::all_of(std::views::values(other.working_callbacks), std::identity{})
+			std::ranges::all_of(std::views::values(other.callbacks), [](auto&& cb) { return static_cast<bool>(cb); })
 			&& "Can not move from an async_signal_handler with running callbacks"
 		);
 
@@ -184,10 +178,8 @@ public:
 		}
 
 		callbacks = container_type{allocator};
-		working_callbacks = working_container_type{allocator};
 		to_remove = remove_container_type{allocator};
 
-		// working_callbacks is not moved from, as moving from a signal handler with working callbacks is not allowed.
 		callbacks = std::move(other.callbacks);
 		to_remove = std::move(other.to_remove);
 	}
@@ -204,7 +196,7 @@ public:
 		auto locks = std::scoped_lock{other.callback_mut, other.to_remove_mut};
 
 		assert(
-			std::ranges::all_of(std::views::values(other.working_callbacks), std::identity{})
+			std::ranges::all_of(std::views::values(other.callbacks), [](auto&& cb) { return static_cast<bool>(cb); })
 			&& "Can not move from an async_signal_handler with running callbacks"
 		);
 
@@ -214,7 +206,6 @@ public:
 			allocator = std::move(other.allocator);
 		}
 
-		// working_callbacks is not moved from, as moving from a signal handler with working callbacks is not allowed.
 		callbacks = std::move(other.callbacks);
 		to_remove = std::move(other.to_remove);
 	}
@@ -236,7 +227,7 @@ public:
 		auto locks = std::scoped_lock{callback_mut, to_remove_mut, other.callback_mut, other.to_remove_mut};
 
 		assert(
-			std::ranges::all_of(std::views::values(working_callbacks), std::identity{})
+			std::ranges::all_of(std::views::values(callbacks), [](auto&& cb) { return static_cast<bool>(cb); })
 			&& "Can not copy to an async_signal_handler with running callbacks"
 		);
 
@@ -273,12 +264,12 @@ public:
 		auto locks = std::scoped_lock{callback_mut, to_remove_mut, other.callback_mut, other.to_remove_mut};
 
 		assert(
-			std::ranges::all_of(std::views::values(working_callbacks), std::identity{})
+			std::ranges::all_of(std::views::values(callbacks), [](auto&& cb) { return static_cast<bool>(cb); })
 			&& "Can not move to an async_signal_handler with running callbacks"
 		);
 
 		assert(
-			std::ranges::all_of(std::views::values(other.working_callbacks), std::identity{})
+			std::ranges::all_of(std::views::values(other.callbacks), [](auto&& cb) { return static_cast<bool>(cb); })
 			&& "Can not move from an async_signal_handler with running callbacks"
 		);
 
@@ -288,7 +279,6 @@ public:
 			allocator = std::move(other.allocator);
 		}
 
-		// working_callbacks is not moved from, as moving from a signal handler with working callbacks is not allowed.
 		callbacks = std::move(other.callbacks);
 		to_remove = std::move(other.to_remove);
 
@@ -315,13 +305,15 @@ public:
 
 	/// Disconnect all callbacks
 	auto disconnect_all() -> void {
-		auto locks = std::scoped_lock{callback_mut, working_callback_mut};
+		auto callback_lock = std::scoped_lock{callback_mut};
 
 		remove_invalidated_callbacks();
 
+		auto remove_lock = std::scoped_lock{to_remove_mut};
+
 		// Remove all callbacks that aren't running, and mark those that are running for deletion.
 		for (auto it = callbacks.begin(); it != callbacks.end();) {
-			if (!working_callbacks.contains(&(*it))) {
+			if (*it) {
 				it = callbacks.erase(it);
 			}
 			else {
@@ -343,14 +335,8 @@ public:
 	template<typename FunctionT>
 	auto connect(FunctionT&& func) -> connection {
 		auto lock = std::unique_lock{callback_mut};
-		auto const it = callbacks.insert(std::forward<FunctionT>(func));
+		auto const it = callbacks.insert(std::allocate_shared<std::function<function_type>>(allocator, std::forward<FunctionT>(func)));
 		lock.unlock();
-
-		// Add an entry for this callback to the working callback map, with an initial value of false.
-		{
-			auto working_lock = std::scoped_lock{working_callback_mut};
-			working_callbacks[&(*it)] = false;
-		}
 
 		return connection{[weak_self = this->weak_from_this(), ptr = &(*it)] {
 			if (auto self = weak_self.lock()) {
@@ -368,9 +354,9 @@ public:
 	{
 		auto pending_callbacks = prune_callbacks_and_get_pending();
 
-		for (auto ptr : pending_callbacks) {
-			(*ptr)(args...);
-			release_callback(ptr);
+		for (auto& [ptr, callback] : pending_callbacks) {
+			(*callback)(args...);
+			release_callback(ptr, std::move(callback));
 		}
 	}
 
@@ -388,9 +374,9 @@ public:
 		auto results = std::vector<ReturnT>{};
 		results.reserve(pending_callbacks.size());
 
-		for (auto ptr : pending_callbacks) {
-			results.emplace_back((*ptr)(args...));
-			release_callback(ptr);
+		for (auto& [ptr, callback] : pending_callbacks) {
+			results.emplace_back((*callback)(args...));
+			release_callback(ptr, std::move(callback));
 		}
 
 		return results;
@@ -408,11 +394,12 @@ public:
 		// Delete callbacks marked for removal, and get a list of those that can be executed. No lock on callback_mut
 		// needs to be acquired for the rest of this function. This is because the callbacks will be marked as running
 		// and thus can not be deleted until released via release_callback().
-		auto const pending_callbacks = prune_callbacks_and_get_pending();
+		auto pending_callbacks = prune_callbacks_and_get_pending();
 
-		for (auto ptr : pending_callbacks) {
-			boost::asio::post(executor, [self = this->shared_from_this(), ptr, args_tuple]() mutable {
-				(void)std::apply(*ptr, *args_tuple);
+		for (auto& [ptr, callback] : pending_callbacks) {
+			boost::asio::post(executor, [self = this->shared_from_this(), ptr, callback = std::move(callback), args_tuple]() mutable {
+				(void)std::apply(*callback, *args_tuple);
+				release_callback(ptr, std::move(callback));
 			});
 		}
 	}
@@ -427,21 +414,21 @@ public:
 	auto async_publish(ArgsT... args, CompletionToken&& completion) {
 		auto args_tuple = std::make_shared<std::tuple<ArgsT...>>(std::forward<ArgsT>(args)...);
 
-		auto const pending_callbacks = prune_callbacks_and_get_pending();
+		auto pending_callbacks = prune_callbacks_and_get_pending();
 
 		// This will post a function which will invoke the callback then re-add the callback to the list of pending
 		// callbacks once it has completed. The actual operation is deferred to be later executed as part of a
 		// parallel_group.
-		auto post_op = [this, &args_tuple](typename container_type::pointer ptr) {
-			auto execute = [this, ptr, &args = *args_tuple]() mutable {
+		auto post_op = [this, &args_tuple](typename container_type::pointer ptr, element_type&& callback) {
+			auto execute = [this, ptr, callback = std::move(callback), &args = *args_tuple]() mutable {
 				if constexpr (std::same_as<void, ReturnT>) {
-					std::apply(*ptr, args);
-					release_callback(ptr);
+					std::apply(*callback, args);
+					release_callback(ptr, std::move(callback));
 					return boost::asio::deferred_t::values(std::monostate{});  //needs to return a value
 				}
 				else {
-					auto result = std::apply(*ptr, args);
-					release_callback(ptr);
+					auto result = std::apply(*callback, args);
+					release_callback(ptr, std::move(callback));
 					return boost::asio::deferred_t::values(std::move(result));
 				}
 			};
@@ -450,12 +437,12 @@ public:
 		};
 
 		// Create the deferred callback invocation for each callback
-		using post_op_type = decltype(post_op({}));
+		using post_op_type = decltype(post_op(std::declval<typename container_type::pointer>(), std::declval<element_type&&>()));
 		auto operations = std::vector<post_op_type>{};
 		operations.reserve(pending_callbacks.size());
 
-		for (auto ptr : pending_callbacks) {
-			operations.emplace_back(post_op(ptr));
+		for (auto& [ptr, callback] : pending_callbacks) {
+			operations.emplace_back(post_op(ptr, std::move(callback)));
 		}
 
 		// Keep this signal handler and the arguments alive until the completion token has been called (when all
@@ -475,18 +462,18 @@ private:
 		to_remove.push_back(pointer);
 	}
 
-	auto release_callback(typename container_type::const_pointer pointer) -> void {
-		auto lock = std::shared_lock{working_callback_mut};
-		working_callbacks[pointer] = false;
+	auto release_callback(typename container_type::pointer pointer, element_type&& callback) -> void {
+		auto lock = std::shared_lock{callback_mut};
+		(*callbacks.get_iterator(pointer)) = std::move(callback);
 	}
 
 	// Remove invalidated callbacks and get a list of the pending ones
 	[[nodiscard]]
-	auto prune_callbacks_and_get_pending() -> std::vector<typename container_type::pointer> {
+	auto prune_callbacks_and_get_pending() -> std::vector<std::pair<typename container_type::pointer, element_type>> {
 		// This will be populated with the callbacks that need to be executed
-		auto pending_callbacks = std::vector<typename container_type::pointer>{};
+		auto pending_callbacks = std::vector<std::pair<typename container_type::pointer, element_type>>{};
 
-		auto working_lock = std::scoped_lock{callback_mut, working_callback_mut};
+		auto lock = std::scoped_lock{callback_mut};
 
 		// Remove invalidated callbacks that aren't currently executing
 		remove_invalidated_callbacks();
@@ -496,30 +483,28 @@ private:
 		// be released during this block since an exclusive mutex is held the entire time. Thus, they won't be included
 		// in this update and will be removed with the next update.
 		for (auto& callback : callbacks) {
-			if (working_callbacks[&callback]) {
-				//SPDLOG_LOG_TRACE("Callback {:#X} dropped a signal", static_cast<uintptr_t>(&(*it)));
+			if (callback) {
+				pending_callbacks.emplace_back(&callback, std::move(callback));
 			}
 			else {
-				pending_callbacks.push_back(&callback);
-				working_callbacks[&callback] = true;
+				//SPDLOG_LOG_TRACE("Callback {:#X} dropped a signal", static_cast<uintptr_t>(&callback));
 			}
 		}
 
 		return pending_callbacks;
 	}
 
-	// Requires an exclusive lock on callback_mut and working_callback_mut
+	// Requires an exclusive lock on callback_mut
 	auto remove_invalidated_callbacks() -> void {
 		auto lock = std::scoped_lock{to_remove_mut};
 
 		// Move all handles whose callbacks aren't currently executing to the end of the vector
 		auto const remove_begin = std::partition(to_remove.begin(), to_remove.end(), [&](auto pointer) {
-			return working_callbacks[pointer];
+			return static_cast<bool>(*pointer); //if the shared_ptr is null, then the callback is in progress
 		});
 
 		// Erase all callbacks that aren't in progress
 		for (auto it = remove_begin; it != to_remove.end(); ++it) {
-			working_callbacks.erase(*it);
 			callbacks.erase(callbacks.get_iterator(*it));
 		}
 
@@ -534,16 +519,9 @@ private:
 	ExecutorT executor;
 
 	container_type callbacks{allocator};
-	std::mutex callback_mut;
+	std::shared_mutex callback_mut;
 
-	// Tracks the execution state of each callback
-	working_container_type working_callbacks{allocator};
-	std::shared_mutex working_callback_mut;
-
-	// Tracks which callbacks have been disconnected, so they can be removed when they are no longer executing. This
-	// part could be removed if callbacks were stored as std::shared_ptr<std::function<...>>, at the cost of extra
-	// indirection, but that doesn't simplify the design too much since the callback execution state has to be tracked
-	// as well. Manually managing the callback lifetime is only a little extra work.
+	// Tracks which callbacks have been disconnected, so they can be removed when they are no longer executing.
 	remove_container_type to_remove{allocator};
 	std::mutex to_remove_mut;
 };
