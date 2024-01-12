530107ff5870e848023d85cc97873e87e1d28e6c
Connor Broyles <cbroyles94@gmail.com>
2024-01-11 23:28:18 -0500

    synchronized_signal_handler now allows connecting callbacks during publishing and within running callbacks

--- include/events/signal_handler/synchronized_signal_handler.hpp
+++ include/events/signal_handler/synchronized_signal_handler.hpp
@@ -1,9 +1,12 @@
 #pragma once
 
+#include <atomic>
 #include <concepts>
 #include <functional>
 #include <mutex>
 #include <shared_mutex>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 #include <plf_colony.h>
@@ -35,9 +38,15 @@ private:
 	using container_allocator_type = typename alloc_traits::template rebind_alloc<element_type>;
 	using container_type = plf::colony<element_type, container_allocator_type>;
 
-	using erase_element_type = typename container_type::const_pointer;
-	using erase_container_allocator_type = typename alloc_traits::template rebind_alloc<erase_element_type>;
-	using erase_container_type = std::vector<erase_element_type, erase_container_allocator_type>;
+	using handle_type = uint64_t;
+	using handle_container_allocator_type = typename alloc_traits::template rebind_alloc<std::pair<const handle_type, typename container_type::const_pointer>>;
+	using handle_container_type = std::unordered_map<handle_type, typename container_type::const_pointer, std::hash<handle_type>, std::equal_to<handle_type>, handle_container_allocator_type>;
+
+	using add_container_allocator_type = typename alloc_traits::template rebind_alloc<std::pair<handle_type, element_type>>;
+	using add_container_type = std::vector<std::pair<handle_type, element_type>, add_container_allocator_type>;
+
+	using erase_container_allocator_type = typename alloc_traits::template rebind_alloc<handle_type>;
+	using erase_container_type = std::vector<handle_type, erase_container_allocator_type>;
 
 public:
 	synchronized_signal_handler() = default;
@@ -52,8 +61,13 @@ public:
 	 *          handler.
 	 */
 	synchronized_signal_handler(synchronized_signal_handler const& other) {
-		auto lock = std::scoped_lock{other.callback_mut};
+		auto lock = std::scoped_lock{other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut};
+
+		other.erase_expired_callbacks_impl();
+		other.add_pending_callbacks_impl();
+
 		callbacks = other.callbacks;
+		next_handle = other.next_handle;
 	}
 
 	/**
@@ -63,9 +77,13 @@ public:
 	 *          handler.
 	 */
 	synchronized_signal_handler(synchronized_signal_handler const& other, AllocatorT const& alloc) : to_erase(alloc) {
-		auto locks = std::scoped_lock{other.callback_mut, other.erase_mut};
+		auto locks = std::scoped_lock{other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut};
+
 		other.erase_expired_callbacks_impl();
+		other.add_pending_callbacks_impl();
+
 		callbacks = container_type{other.callbacks, alloc};
+		next_handle = other.next_handle;
 	}
 
 	/**
@@ -74,9 +92,13 @@ public:
 	 * @details Existing connection objects from the original signal handler are invalidated.
 	 */
 	synchronized_signal_handler(synchronized_signal_handler&& other) {
-		auto locks = std::scoped_lock{other.callback_mut, other.erase_mut};
-		to_erase = std::move(other.to_erase);
+		auto locks = std::scoped_lock{other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut};
+
 		callbacks = std::move(other.callbacks);
+		handles = std::move(other.handles);
+		to_add = std::move(other.to_add);
+		to_erase = std::move(other.to_erase);
+		next_handle = other.next_handle;
 	}
 
 	/**
@@ -85,9 +107,13 @@ public:
 	 * @details Existing connection objects from the original signal handler are invalidated.
 	 */
 	synchronized_signal_handler(synchronized_signal_handler&& other, AllocatorT const& alloc) {
-		auto locks = std::scoped_lock{other.callback_mut, other.erase_mut};
-		to_erase = erase_container_type{std::move(other.to_erase), alloc};
+		auto locks = std::scoped_lock{other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut};
+
 		callbacks = container_type{std::move(other.callbacks), alloc};
+		handles = handle_container_type{std::move(other.handles), alloc};
+		to_add = add_container_type{std::move(other.to_add), alloc};
+		to_erase = erase_container_type{std::move(other.to_erase), alloc};
+		next_handle = other.next_handle;
 	}
 
 	~synchronized_signal_handler() = default;
@@ -103,9 +129,16 @@ public:
 			return *this;
 		}
 
-		auto locks = std::scoped_lock{callback_mut, other.callback_mut, other.erase_mut};
+		auto locks = std::scoped_lock{
+			callback_mut, handle_mut, add_mut, erase_mut,
+			other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut
+		};
+
 		other.erase_expired_callbacks_impl();
+		other.add_pending_callbacks_impl();
+
 		callbacks = other.callbacks;
+		next_handle = other.next_handle;
 
 		return *this;
 	}
@@ -119,9 +152,16 @@ public:
 			return *this;
 		}
 
-		auto locks = std::scoped_lock{callback_mut, erase_mut, other.callback_mut, other.erase_mut};
+		auto locks = std::scoped_lock{
+			callback_mut, handle_mut, add_mut, erase_mut,
+			other.callback_mut, other.handle_mut, other.add_mut, other.erase_mut
+		};
+
 		callbacks = std::move(other.callbacks);
+		handles = std::move(other.handles);
+		to_add = std::move(other.to_add);
 		to_erase = std::move(other.to_erase);
+		next_handle = other.next_handle;
 
 		return *this;
 	}
@@ -139,9 +179,8 @@ public:
 	}
 
 	/**
-	 * @brief Register a callback function that will be invoked when the signal is fired
-	 *
-	 * @tparam FunctionT
+	 * @brief Register a callback function that will be invoked when the signal is fired. If the callback cannot be
+	 *        inserted immediately, then it will be enqueued for later insertion.
 	 *
 	 * @param callback  A function that is compatible with the signal handler's function signature
 	 *
@@ -149,11 +188,28 @@ public:
 	 */
 	template<std::invocable<ArgsT...> FunctionT>
 	auto connect(FunctionT&& callback) -> connection {
-		auto lock = std::unique_lock{callback_mut};
-		auto const it = callbacks.insert(std::forward<FunctionT>(callback));
-		lock.unlock();
+		auto const handle = next_handle.fetch_add(1);
+		auto callback_ptr = typename container_type::const_pointer{nullptr};
 
-		return connection{[this, ptr = &(*it)] { disconnect(ptr); }};
+		{
+			auto callback_lock = std::unique_lock{callback_mut, std::try_to_lock};
+
+			if (callback_lock) {
+				auto const it = callbacks.insert(std::forward<FunctionT>(callback));
+				callback_ptr = &(*it);
+			}
+			else {
+				auto add_lock = std::scoped_lock{add_mut};
+				to_add.emplace_back(handle, std::forward<FunctionT>(callback));
+			}
+		}
+
+		{
+			auto handle_lock = std::scoped_lock{handle_mut};
+			handles[handle] = callback_ptr;
+		}
+
+		return connection{[this, handle] { disconnect(handle); }};
 	}
 
 	/// Disconnect all callbacks
@@ -172,10 +228,15 @@ public:
 	{
 		erase_expired_callbacks();
 
-		auto lock = std::shared_lock{callback_mut};
-		for (auto& callback : callbacks) {
-			callback(args...);
+		{
+			auto lock = std::shared_lock{callback_mut};
+
+			for (auto& callback : callbacks) {
+				callback(args...);
+			}
 		}
+
+		add_pending_callbacks();
 	}
 
 	/**
@@ -189,41 +250,92 @@ public:
 	{
 		erase_expired_callbacks();
 
-		auto lock = std::shared_lock{callback_mut};
-
 		auto results = std::vector<ReturnT>{};
 		results.reserve(callbacks.size());
 
-		for (auto& callback : callbacks) {
-			results.emplace_back(callback(args...));
+		{
+			auto lock = std::shared_lock{callback_mut};
+
+			for (auto& callback : callbacks) {
+				results.emplace_back(callback(args...));
+			}
 		}
+
+		add_pending_callbacks();
 
 		return results;
 	}
 
 private:
-	auto disconnect(typename container_type::const_pointer callback_pointer) -> void {
+	auto disconnect(handle_type handle) -> void {
 		auto lock = std::scoped_lock{erase_mut};
-		to_erase.push_back(callback_pointer);
+		to_erase.push_back(handle);
+	}
+
+	auto add_pending_callbacks() -> void {
+		auto add_lock = std::scoped_lock{add_mut};
+		if (to_add.empty()) {
+			return;
+		}
+
+		auto locks = std::scoped_lock{callback_mut, handle_mut};
+		add_pending_callbacks_impl();
+	}
+
+	auto add_pending_callbacks_impl() -> void {
+		for (auto& [handle, callback] : to_add) {
+			auto const it = handles.find(handle);
+
+			// If the handle doesn't exist in the handle map, then this callback was disconnected before it could be
+			// inserted into the callback list. In this case, just skip it.
+			if (it != handles.end()) {
+				continue;
+			}
+
+			auto const callback_it = callbacks.insert(std::move(callback));
+			it->second = &(*callback_it);
+		}
+
+		to_add.clear();
 	}
 
 	auto erase_expired_callbacks() -> void {
-		auto locks = std::scoped_lock{callback_mut, erase_mut};
+		auto erase_lock = std::scoped_lock{erase_mut};
+		if (to_erase.empty()) {
+			return;
+		}
+
+		auto locks = std::scoped_lock{callback_mut, handle_mut};
 		erase_expired_callbacks_impl();
 	}
 
-	// Requires an exclusive lock on callback_mut and erase_mut
+	// Requires an exclusive lock on callback_mut, handle_mut, and erase_mut
 	auto erase_expired_callbacks_impl() -> void {
-		for (auto callback_pointer : to_erase) {
-			callbacks.erase(callbacks.get_iterator(callback_pointer));
+		for (auto handle : to_erase) {
+			auto const it = handles.find(handle);
+
+			// The pointer will be null if the callback is still pending insertion. Nothing special needs to be done
+			// in this case other than skipping the actual callback deletion. This case will be detected and the
+			// pending callback will be deleted when attempting to add the enqueued callbacks.
+			if (it->second) {
+				callbacks.erase(callbacks.get_iterator(it->second));
+			}
+
+			handles.erase(it);
 		}
 
 		to_erase.clear();
 	}
 
-
 	container_type callbacks;
 	std::shared_mutex callback_mut;
+
+	handle_container_type handles;
+	std::atomic<handle_type> next_handle = 0;
+	std::mutex handle_mut;
+
+	add_container_type to_add;
+	std::mutex add_mut;
 
 	erase_container_type to_erase;
 	std::mutex erase_mut;
